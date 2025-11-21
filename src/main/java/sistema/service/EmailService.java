69532e6cf3599007f86bdb400fa19d551a9184c0
jalmir <jalmirwinter@gmail.com>
2025-11-21 04:34:36 -0300

    Refactor email service to use Sendinblue API and update configuration properties

--- src/main/java/sistema/service/EmailService.java
+++ src/main/java/sistema/service/EmailService.java
@@ -1,9 +1,8 @@
 package sistema.service;
 
-import lombok.AllArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import sibApi.TransactionalEmailsApi;
 import sibModel.SendSmtpEmail;
 import sibModel.SendSmtpEmailSender;
@@ -12,33 +11,7 @@ import sibModel.SendSmtpEmailTo;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class EmailService {
-//    private final JavaMailSender mailSender;
-//
-//    public void sendActivationEmail(String to, String activationLink) throws MessagingException {
-//        MimeMessage message = mailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-//        helper.setTo(to);
-//        helper.setSubject("Ative sua conta - Clique no link");
-//        helper.setText("<h3>Bem-vindo!</h3>"
-//                        + "<p>Clique no link abaixo para ativar sua conta:</p>"
-//                        + "<p><a href=\"" + activationLink + "\">Ative sua conta</a></p>"
-//                        + "<p>Se você não solicitou este cadastro, por favor ignore este e-mail.</p>",
-//                true);
-//        mailSender.send(message);
-//    }
-//
-//    public void sendPasswordResetEmail(String to, String resetLink) throws MessagingException {
-//        MimeMessage message = mailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-//        helper.setTo(to);
-//        helper.setSubject("Redefinição de Senha");
-//        helper.setText("<p>Clique no link abaixo para redefinir sua senha:</p>"
-//                + "<p><a href=\"" + resetLink + "\">Redefinir Senha</a></p>"
-//                + "<p>Se você não solicitou esta redefinição, por favor ignore este email.</p>", true);
-//        mailSender.send(message);
-//    }
 
     @Value("${brevo.api.key}")
     private String brevoApiKey;
